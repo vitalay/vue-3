cfbb186dbad2be2672ce928764c57f7d225584e1
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-12 14:09:45 +0500

    ПРОКИДОВАНИЕ ПРОПСОВ  PROPS

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,16 +2,34 @@
 
 <template>
   <div class="container-fluid">
-    <h1>Hello</h1>
+  <h1>App</h1>
 <my-component />
+<MyNotes v-for="note in notes" v-bind:key="note" v-bind:oneNote="note"   />  
   </div>
   
 
 </template>
 
 
+<script>
+import MyNotes from './MyNotes.vue';
 
-<script setup>
+ 
+export default {
+
+  data() {
+   
+    return {
+     notes: ['Почистить зубы', 'Покушать', 'Сходить на работу'] //notes примичание
+    }
+  },
+
+   components: {
+     MyNotes
+   }
+   
+ }
+
 
 </script>
  
---
+++ vue-project/src/MyNotes.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    
+    <h2>Заметка: {{ oneNote }}</h2>
+    <strong>Прочитано</strong>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['oneNote'],
+    data() {
+        return {
+    };
+    },
+
+
+}
+</script>
+
+<style>
+
+</style>
